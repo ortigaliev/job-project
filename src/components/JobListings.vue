2962e18a4e87a14338a6765e35efb997f9b9b95b
Muhammadjon Ortigaliyev <ortigaliyevm@gmail.com>
2024-11-13 18:04:04 +0900

    feat: Publishing HomeCard and JobListings Components

---
+++ src/components/JobListings.vue
@@ -0,0 +1,11 @@
+<script setup>
+import jobData from '@/jobs.json'
+import { ref } from 'vue';
+
+const jobs = ref(jobData);
+console.log(jobs.value)
+</script>
+
+<template>
+    Job Listings
+</template>
