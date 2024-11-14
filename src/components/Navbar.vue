62737848f27358cdc71bad295d5651c55592be2f
Muhammadjon Ortigaliyev <ortigaliyevm@gmail.com>
2024-11-14 20:24:06 +0900

    feat: Develop Add Job logic

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -26,7 +26,7 @@ const isActiveLink = (routePath) => {
               </RouterLink>
               <RouterLink to="/jobs"
                 :class="[isActiveLink('/jobs') ? 'bg-green-900' : 'hover:bg-gray-900', 'text-white', 'px-3', 'py-2', 'rounded-md']">
-                Home
+                Jobs
               </RouterLink>
               <RouterLink to="/jobs/add"
                 :class="[isActiveLink('/jobs/add') ? 'bg-green-900' : 'hover:bg-gray-900', 'text-white', 'px-3', 'py-2', 'rounded-md']">
